7012c6f5a915bbb0f8e8c9ec20b2f0e341d38897
akshat111 <kakshat111@gmail.com>
2024-07-07 21:06:15 +0530

    Update script.js

--- IntelliChat/script.js
+++ IntelliChat/script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "Put your own api";
+const API_KEY = "sk-proj-hlqXHQRKQzWyzAWL2LpGT3BlbkFJmHSAhJyVaZYWmaR3UvGu";
 const initialHeight = chatInput.scrollHeight;
 
 const loadDataFromLocalstorage = () => {
